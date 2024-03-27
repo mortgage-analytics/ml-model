86dbd68138db103bc52e3eaa5e95efbde42ae015
Hartnero <hartnero@tcd.ie>
2024-03-27 20:03:50 +0000

    Updated Data analytics code for new data given

--- Scripts/Client.py
+++ Scripts/Client.py
@@ -1,19 +1,30 @@
 # from Timeline import Timeline
 
 class Client:
-    def __init__(self, type, submitted_to, property_identification, mortgage_amount, single_joint, application):
-        self.type = type
-        self.submitted_to = submitted_to
-        self.property_identification = property_identification
-        self.mortgage_amount = mortgage_amount
-        self.single_joint = single_joint
+    def __init__(self, client_type, aip_lender_submitted_to, property_value, mortgage_amount_proposed,
+                  application_type, mortgage_type,application_stage,application_status,documents_uploaded,
+                  aip_amount,fact_find_surplus_per_month,sum_of_income,application):
+        self.client_type = client_type
+        self.aip_lender_submitted_to = aip_lender_submitted_to
+        self.property_value = property_value
+        self.mortgage_amount_proposed = mortgage_amount_proposed
+        self.application_type = application_type
         self.application = application
+        self.mortgage_type = mortgage_type
+        self.application_stage = application_stage
+        self.application_status = application_status
+        self.documents_uploaded = documents_uploaded
+        self.aip_amount = aip_amount
+        self.fact_find_surplus_per_month =fact_find_surplus_per_month
+        self.sum_of_income = sum_of_income
+
 
     def display_info(self):
         """Prints the client's information."""
-        print(f"Client Type: {self.type}")
-        print(f"Submitted To: {self.submitted_to}")
-        print(f"Property Identification: {self.property_identification}")
-        print(f"Mortgage Amount: {self.mortgage_amount}")
+        print(f"Client Type: {self.client_type}")
+        print(f"Submitted To: {self.aip_lender_submitted_to}")
+        print(f"Property Value: {self.property_value}")
+        print(f"Mortgage Amount Proposed: {self.mortgage_amount_proposed}")
+        print(f"Application Status: {self.application_status}")
         print(f"Application")
 
--- Scripts/data_analytics.py
+++ Scripts/data_analytics.py
@@ -1,4 +1,7 @@
 import numpy as np
+from Timeline import Timeline
+from datetime import datetime
+
 # from Scripts.Timeline import calculate_differences
 
 def getStage(applications, stageName):
@@ -21,28 +24,43 @@ def rankForEachStage(applications, stage):
     return sorted(stageArray), population
 
 def WhatType(clients):
-    FTBCount = 0
-    STBCount = 0
-    SwitchingCount = 0
-    EquityCount = 0
+    AIP_Expected_Count = 0
+    AIP_Granted_Count = 0
+    Closed_Count = 0
+    InfoGathering_Count = 0
+    LoanOfferRec_Count = 0
+    Not_Proceeding_Count = 0
+    OnHold_Count = 0
+    PreCompletion_Count = 0
+    PreLoanOffer_Count = 0
     for client in clients:
-        if client.type == "First Time Buyer":
-            FTBCount += 1
-        elif client.type == "Second Time Buyer":
-            STBCount += 1
-        elif client.type == "Switching":
-            SwitchingCount += 1
-        else:
-            EquityCount += 1
-    return FTBCount, STBCount, SwitchingCount, EquityCount
+        if client.application_status == "AIP expected":
+            AIP_Expected_Count += 1
+        elif client.application_status == "AIP granted":
+            AIP_Granted_Count += 1
+        elif client.application_status == "Closed":
+            Closed_Count += 1
+        elif client.application_status == "Information gathering":
+            InfoGathering_Count += 1
+        elif client.application_status == "Loan Offer received":
+            LoanOfferRec_Count += 1
+        elif client.application_status == "Not proceeding":
+            Not_Proceeding_Count += 1
+        elif client.application_status == "On hold":
+            OnHold_Count += 1
+        elif client.application_status == "Pre-completion":
+            PreCompletion_Count += 1
+        elif client.application_status == "Pre-Loan Offer":
+            PreLoanOffer_Count += 1
+    return AIP_Expected_Count,AIP_Granted_Count,Closed_Count,InfoGathering_Count,LoanOfferRec_Count,Not_Proceeding_Count,OnHold_Count,PreCompletion_Count,PreLoanOffer_Count
 
 def SingleOrJoint(clients):
     singleCount = 0
     jointCount = 0
     for client in clients:
-        if client.single_joint == "Single":
+        if client.application_type == "Single":
             singleCount += 1
-        elif client.single_joint == "Joint":
+        elif client.application_type == "Joint":
             jointCount += 1
     return singleCount,jointCount
 
@@ -50,19 +68,81 @@ def WhatBank(clients):
     ICSCount = 0
     HavenCount = 0
     BOICount = 0
+    AvantCount = 0
+    FinanceIrelandCount = 0
+    KBCCount = 0
     OtherCount = 0
+    PermanentTSBCOunt = 0
+    UlsterCount = 0
     for client in clients:
-        if client.submitted_to == "ics":
+        if client.aip_lender_submitted_to == "ICS Mortgages":
             ICSCount += 1
-        elif client.submitted_to == "haven":
+        elif client.aip_lender_submitted_to == "Haven":
             HavenCount += 1
-        elif client.submitted_to == "boi":
+        elif client.aip_lender_submitted_to == "Bank of Ireland":
             BOICount += 1
+        elif client.aip_lender_submitted_to == "Avant Money":
+            AvantCount += 1
+        elif client.aip_lender_submitted_to == "Finance Ireland":
+            FinanceIrelandCount += 1
+        elif client.aip_lender_submitted_to == "KBC":
+            KBCCount += 1
+        elif client.aip_lender_submitted_to == "Ulster Bank":
+            UlsterCount += 1
+        elif client.aip_lender_submitted_to == "Permanent TSB":
+            UlsterCount += 1
         else:
             OtherCount += 1
-    return ICSCount,HavenCount,BOICount,OtherCount
+    return ICSCount,HavenCount,BOICount,AvantCount,FinanceIrelandCount,KBCCount,OtherCount,PermanentTSBCOunt,UlsterCount
 
+def IncomeOfCompletedMortgages(clients):
+    averages = []
+    lowest = float('inf')  # Use infinity as the initial lowest value
+    highest = 0  # Initialize highest as 0, assuming all incomes are positive
 
+    for client in clients:
+        if client.application_status == "Closed" and client.application_stage == "COMPLETE":
+            income = client.sum_of_income
+            if income is not None and not np.isnan(income) and income > 0:
+                averages.append(income)
+                if income < lowest:
+                    lowest = income
+                if income > highest:
+                    highest = income
+    if averages:
+        mean = np.nanmean(averages)
+        std_dev = np.nanstd(averages, ddof=1)  
+        lowest = None if lowest == float('inf') else lowest
+    else:
+        mean, std_dev = float('nan'), float('nan')
+        lowest, highest = 'No data', 'No data'
+    return mean, std_dev, highest, lowest
+
+def _num_of_days2(date1, date2):
+    date_format = "%d/%m/%Y"  # Ensure the format matches your date strings
+    try:
+        a = datetime.strptime(date1, date_format)
+        b = datetime.strptime(date2, date_format)
+        return (b - a).days
+    except ValueError:
+        return None
+
+def AverageTimeToCompletion(clients):
+    daysToCompletion = []
+    for client in clients:
+        lead_created_date = client.application.lead_created_date
+        offer_letter_received_date = client.application.offer_letter_received_date
+        if lead_created_date and isinstance(offer_letter_received_date, str) and offer_letter_received_date.lower() != 'nan':
+            duration = _num_of_days2(lead_created_date, offer_letter_received_date)
+            if duration is not None:  # Ensure duration is a valid number
+                daysToCompletion.append(duration)
+    valid_durations = [day for day in daysToCompletion if day is not None]
+    if valid_durations:
+        mean = np.mean(valid_durations)  # Now you can directly use np.mean
+        std_dev = np.std(valid_durations)
+        return f"{mean:.2f}",f"{std_dev:.2f}"
+    else:
+        return 'No data'
 # getStage:
 #   Takes in an array of type Application and the stage of the application
 #   we are looking for as a string. 
