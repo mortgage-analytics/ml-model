a56e0b8ea930dc63612f287e5896e3087dfd38f1
Andrew Furey <andrewfurey2003@gmail.com>
2024-04-02 13:25:59 +0100

    update

--- models/helpers.py
+++ models/helpers.py
@@ -7,7 +7,10 @@ def log_prob(x, mean, variance):
 def normal_prob(x, mean, variance):
     return (x-mean) / sqrt(variance)
 
-def gauss_prob(x, mean, variance):
-    return (1/sqrt(2 * pi) * (variance**2)) * exp(-((x-mean)**2)/(2*(variance**4)))
+# def gauss_prob(x, mean, variance):
+#     return (1/sqrt(2 * pi) * (variance**2)) * exp(-((x-mean)**2)/(2*(variance**4)))
+def gauss_prob(x, mean, stdev):
+    exponent = exp(-((x-mean)**2 / (2 * stdev**2 )))
+    return (1 / (sqrt(2 * pi) * stdev)) * exponent
 
 def getenv(key, default=0): return type(default)(os.getenv(key, default))
--- models/model.py
+++ models/model.py
@@ -32,21 +32,26 @@ class Model:
 
         self.feature_variances = self.feature_variances / (np.reshape(np.repeat(self.counts, self.features), newshape=(self.classes, self.features))-1)
 
+        #for i in range(self.features):
+        #    self.feature_variances[0][i] = self.feature_variances[0][i] / self.probabilities[0]
+        #    self.feature_variances[1][i] = self.feature_variances[1][i] / self.probabilities[1]
+
+        #self.feature_variances = self.feature_variances /
+        print(self.feature_means)
 
     def classify(self, data:np.ndarray) -> Tuple[int, float]:
         """
         Gives a prediction given the data, with a probability
         """
+        output = (0, float("-inf"))
         assert self.features == len(data), "Number of features of model does not match input data"
-        output = (-1, float("-inf"))
 
         #TODO: there is definitly a better way to do this
         for i in range(self.classes):
-            #prob = self.probabilities[i]
-            prob = 1
+            prob = self.probabilities[i]
             for j in range(self.features):
-                prob *= gauss_prob(data[j], self.feature_means[i][j], self.feature_variances[i][j])
-
+                prob *= gauss_prob(data[j], self.feature_means[i][j], sqrt(self.feature_variances[i][j]))
+            #print(prob, i)
             if (prob > output[1]):
                 output = (i, prob)
 
@@ -63,7 +68,7 @@ class Model:
 def load(file_name:str) -> Model:
     """
        Imports feature means and variances in a file
-       """
+    """
     pass
 
 if __name__ == "__main__":
@@ -75,7 +80,7 @@ if __name__ == "__main__":
     training_data = data[:training_size]
     testing_data = data[training_size:]
 
-    model = Model(features=6, classes=2)
+    model = Model(features=5, classes=2)
     print("Training model...")
     model.train(training_data)
 
@@ -87,14 +92,18 @@ if __name__ == "__main__":
         actual = model.classify(testing_data[i,:-1])[0]
         vals[actual] += 1
         actual_vals[expected] += 1
-        #print(actual, expected)
         if (actual == expected):
             count += 1
     accuracy = count/len(testing_data)
 
-    print("Guesses:",vals)
-    print("Actual:",actual_vals)
-    print(f"Accuracy: {accuracy*100:.2f}%")
+    #print("Guesses:",vals)
+    #print("Actual:",actual_vals)
+    #print(f"Accuracy: {accuracy*100:.2f}%")
+
+    #point = np.array([1, 0, 380000, 340000, 1])
+    #point = np.array([1, 0, 595000, 514000, 1])#should be a 1
+    #g = model.classify(point)
+    #print(g, 1)
 
     if getenv("EXPORT") == 1:
         model.save("model_file")
